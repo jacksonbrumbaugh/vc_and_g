66736293c79ec3dc0d4ecf4fb433318ea0575db6
Jackson Brumbaugh <jcb5582@gmail.com>
2024-04-25 02:01:58 -0400

    Make New-OrderInfo

---
+++ MainFunctions/New-OrderInfo.ps1
@@ -0,0 +1,12 @@
+<# RULER ----15--------25-----------------------50----------------------------80                 100                 120
+Modified: 2024-04-24
+By: Jackson B
+#>
+function New-OrderInfo {
+  [CmdletBinding()]
+
+  param () # End block:param
+
+  process {} # End block:process
+
+} # End function
